b781dd70687c4528f3d2cf574aa91328ee749a5c
Raina <2579219825x@gmail.com>
2024-08-25 16:13:27 +0800

    112

--- UnityTools/My project (1)/Assets/Script/All_The_Way/PlayerCar.cs
+++ UnityTools/My project (1)/Assets/Script/All_The_Way/PlayerCar.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCar : MonoBehaviour
 {
-    public float speed;
-    private Vector3 direction;
+    private float speed = 5;
+    private Vector3 distance;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 distance = new Vector3(0,0,0);
-        direction = gameObject.transform.forward;
+        distance = new Vector3(0, 0, 1);
+        if (GetComponent<Rigidbody>())
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
+        }
     }
 
     // Update is called once per framse
     void Update()
     {
-        gameObject.transform.Translate(direction.normalized*Time.deltaTime*speed);
+        gameObject.transform.Translate(distance * Time.deltaTime * speed, Space.World);
+    }
+    void OnCollisionEnter(Collision collision){
+        // Debug.Log("Collision detected with " + collision.gameObject.name);
     }
 }
--- UnityTools/My project (1)/Assets/Script/All_The_Way/RoadBlock.cs
+++ UnityTools/My project (1)/Assets/Script/All_The_Way/RoadBlock.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoadBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+    void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log("Collision detected with " + collision.gameObject.name);
     }
 }
