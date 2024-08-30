fa5bf06b30bbd0b47170290743037accce38725c
Raina <2579219825x@gmail.com>
2024-08-30 17:02:19 +0800

    更换模型

---
+++ UnityTools/My project (1)/Assets/Script/All_The_Way/ABMGR.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABMGR : MonoBehaviour
+{
+    public void LoadRes(string abname, string resName)
+    {
+        
+    }
+}
